a345ee8651f10ded630108a3b9f0baa3bd7db3cd
caseyjramirez <caseyjramirez5@gmail.com>
2023-02-08 18:49:44 -0600

    preview v1

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,30 +1,25 @@
 import braden from '../assets/braden.jpeg';
 import { useNavigate } from "react-router-dom";
-const beerDrinkingPlaylist = 'https://open.spotify.com/playlist/2jxSQenFYXjkBcmNWQukQ9?si=YSGqP-C_T5C4OPj_ewF16Q';
+// const beerDrinkingPlaylist = 'https://open.spotify.com/playlist/2jxSQenFYXjkBcmNWQukQ9?si=YSGqP-C_T5C4OPj_ewF16Q';
 
 const Preview = () => {
-    let navigate = useNavigate();
-
-    const goToBeerDrinkingMusic = () => {
-        navigate(beerDrinkingPlaylist);
-    }
 
     return (
         <div className="full-page-span">
             <div className="flex center-center full-page-height">
                 <div className='preview mt-10'>
 
-                    <div className="text-container mb-20">
+                    {/* <div className="text-container mb-20">
                         <h1 className='large black'>Cheapest Beer In Dallas</h1>
-                    </div>
+                    </div> */}
 
                     <div className="preview-img-container mb-20">
                         <img src={braden} alt="beautiful man holding beer" />
                     </div>
 
-                    <div className="text-container mb-20">
+                    {/* <div className="text-container mb-20">
                         <p className='black'>Please bear with us while we make this house a home. In the meantime might we recommend drinking some cheap beer?</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
